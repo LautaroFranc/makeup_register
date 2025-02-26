b3f629b51287cd5b6d1456d508495ede12f2ba3c
Lautaro Franco <hola12lf@gmail.com>
2025-02-26 12:07:37 -0300

    fix url

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -16,7 +16,7 @@ interface FetchResult<T> {
 
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? process.env.URL_PROD
+    ? process.env.URL_PROD || "https://makeup-register.vercel.app"
     : "http://localhost:3000/";
 
 export function useFetch<T = unknown>(): FetchResult<T> {
